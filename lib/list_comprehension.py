eb43b9ddfbe195b39eaf9470a7a21a20fb314c05
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-01-25 22:22:38 -0500

    Getting acquainted with lambda functions (anonymous functions in js), understanding exactly what pytests are looking for, and acquainted with the helper functions in python like filter, reduce, map, recursive functions, decorators, and memoization.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    #The filter function accepts a function (anon included) and the target list.
+    #The use is filter(customFunction, targetList) which returns a filter object.
+    #Must wrap this with a list statement. list(filter(customFunc, targetList))
+    evens = list(filter(lambda num: num % 2 == 0 and num >= 0, num_list))
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation = lambda s : s + "!"
+    return [exclamation(sentence) for sentence in sentence_list]
+    
